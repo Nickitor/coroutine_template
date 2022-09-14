66fd5c4520f81f2176413200662aa70efaa0d91f
Nikita Zamyslov <38568177+Nickitor@users.noreply.github.com>
2022-09-14 19:35:20 +0300

    Added suspend fun

--- app/src/main/java/com/nikitazamyslov/coroutinetemplate/MainActivity.kt
+++ app/src/main/java/com/nikitazamyslov/coroutinetemplate/MainActivity.kt
@@ -2,10 +2,21 @@ package com.nikitazamyslov.coroutinetemplate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        lifecycleScope.launch(Dispatchers.Default) {
+            doWork()
+        }
+    }
+
+    suspend fun doWork() {
+        delay((500L..5000L).random())
+        println("Done!")
     }
 }
