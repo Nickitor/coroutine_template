7fc6a4e9a74ae8266a321c22e68239e29e7b1eea
Nikita Zamyslov <38568177+Nickitor@users.noreply.github.com>
2022-09-16 19:04:26 +0300

    Added CoroutineName

--- app/src/main/java/com/nikitazamyslov/coroutinetemplate/MainActivity.kt
+++ app/src/main/java/com/nikitazamyslov/coroutinetemplate/MainActivity.kt
@@ -8,45 +8,46 @@ import kotlinx.coroutines.*
 class MainActivity : AppCompatActivity() {
 
     // Обрабатывает все необработанные исключения подобно uncaughtExceptionHandler
-    private val handler = CoroutineExceptionHandler { _, exception ->
-        println("CoroutineExceptionHandler got $exception")
+    private val handler = CoroutineExceptionHandler { context, exception ->
+        println("CoroutineExceptionHandler got $exception from ${context[CoroutineName]?.name}")
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val coroutine: Job = lifecycleScope.launch(Dispatchers.Default + handler) {
-            try {
-                // По истечении времени выбрасывает исключение TimeoutCancellationException
-                // - наследник CancellationException
-                withTimeout(2000) {
-                    if (getData().contains("Some Data!"))
-                        throw IndexOutOfBoundsException()
-                }
-                val data: Deferred<List<String>> = async {
-                    getData()
-                }
-                if (data.await().contains("Some Data!")) {
-                    println("Data error!")
-                    cancel()
-                } else {
-                    println("Data received")
-                }
-                if (isActive) {
-                    doWork()
-                }
-                // catch (e: Exception) { Не работает
-                //     println("Catch")
-                // }
-            } finally {
-                withContext(NonCancellable) { // Позволяет выполнить единый неотменяемый блок
-                    println("exception!")
-                    delay(1000)
-                    println("exception2!")
+        val coroutine: Job =
+            lifecycleScope.launch(CoroutineName("Coroutine#1") + Dispatchers.Default + handler) {
+                try {
+                    // По истечении времени выбрасывает исключение TimeoutCancellationException
+                    // - наследник CancellationException
+                    withTimeout(2000) {
+                        if (getData().contains("Some Data!"))
+                            throw IndexOutOfBoundsException()
+                    }
+                    val data: Deferred<List<String>> = async {
+                        getData()
+                    }
+                    if (data.await().contains("Some Data!")) {
+                        println("Data error!")
+                        cancel()
+                    } else {
+                        println("Data received")
+                    }
+                    if (isActive) {
+                        doWork()
+                    }
+                    // catch (e: Exception) { Не работает
+                    //     println("Catch")
+                    // }
+                } finally {
+                    withContext(NonCancellable) { // Позволяет выполнить единый неотменяемый блок
+                        println("exception!")
+                        delay(1000)
+                        println("exception2!")
+                    }
                 }
             }
-        }
     }
 
     private suspend fun doWork() {
